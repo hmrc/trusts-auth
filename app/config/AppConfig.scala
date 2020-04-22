932891dc0bf70e30def0ae0401a7ca53233d7605
Adam Conder <adamdconder@gmail.com>
2020-04-22 11:09:05 +0100

    Removing dependency on services config

--- app/config/AppConfig.scala
+++ app/config/AppConfig.scala
@@ -18,12 +18,11 @@ package config
 
 import javax.inject.{Inject, Singleton}
 import play.api.Configuration
-import uk.gov.hmrc.play.bootstrap.config.ServicesConfig
 
 @Singleton
-class AppConfig @Inject()(config: Configuration, servicesConfig: ServicesConfig) {
+class AppConfig @Inject()(config: Configuration) {
 
-  val authBaseUrl: String = servicesConfig.baseUrl("auth")
+  val authBaseUrl: String = config.get[Service]("microservice.services.auth").baseUrl
 
   val auditingEnabled: Boolean = config.get[Boolean]("auditing.enabled")
   val graphiteHost: String     = config.get[String]("microservice.metrics.graphite.host")
