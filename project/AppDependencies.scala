44383ce8d2e3c872908c6eebc685e18b56596299
darrylmurphy <darrylmurphy@users.noreply.github.com>
2021-08-11 13:42:48 +0100

    Upgrade to 2.8

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -4,20 +4,20 @@ import sbt._
 object AppDependencies {
 
   val compile: Seq[ModuleID] = Seq(
-
-    "uk.gov.hmrc"             %% "bootstrap-backend-play-27"  % "5.8.0",
+    "uk.gov.hmrc"             %% "bootstrap-backend-play-28"  % "5.1.0",
     "com.typesafe.play"       %% "play-json-joda"             % "2.9.2"
   )
 
   val test: Seq[ModuleID] = Seq(
-    "org.scalatest"           %% "scalatest"                % "3.0.9"                 % "test",
     "com.typesafe.play"       %% "play-test"                % current                 % "test",
+    "org.scalatest"           %% "scalatest"                % "3.0.9"                 % "test",
+    "org.scalatestplus"       %% "mockito-3-4"              % "3.3.0.0-SNAP3"         % "test, it",
     "org.pegdown"             %  "pegdown"                  % "1.6.0"                 % "test, it",
-    "org.scalatestplus.play"  %% "scalatestplus-play"       % "4.0.3"                 % "test, it",
+    "org.scalatestplus.play"  %% "scalatestplus-play"       % "5.1.0"                 % "test, it",
+    "com.vladsch.flexmark"    % "flexmark-all"              % "0.35.10"               % "test, it",
     "com.github.tomakehurst"  %  "wiremock-standalone"      % "2.27.2",
     "org.mockito"             %  "mockito-all"              % "1.10.19",
-    "org.scalacheck"          %% "scalacheck"               % "1.14.3"                % "test",
-    "wolfendale"              %% "scalacheck-gen-regexp"    % "0.1.2"                 % "test"
+    "org.scalacheck"          %% "scalacheck"               % "1.14.3"                % "test"
   )
 
   val akkaVersion = "2.6.7"
